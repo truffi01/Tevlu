2c329202cc95cbeac8f32bd76f036f539c9082f2
Thomas Ruffino <thomasruffino1@gmail.com>
2019-11-06 13:01:04 -0500

    added a create user template

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from .views import homePage
+from .views import HomePage, CreateUser
 
 urlpatterns = [
-    path('', homePage, name='home')
+    path('', homePage, name='home'),
+    path('view/', HomePage.as_view(), name='home'),
+    path('createuser/', CreateUser.as_view(), name = 'createUser')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 def homePage(req):
     return HttpResponse('First App')
+
+class HomePage (TemplateView):
+    template_name = 'home.html'
+
+class CreateUser (TemplateView):
+    template_name = 'createUser.html'
