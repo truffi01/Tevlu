69551a580971f4297245260d38b7e62a7cf8bd4b
Thomas Ruffino <thomasruffino1@gmail.com>
2019-11-07 08:04:41 -0500

    added template view and changed as homepage

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from .views import homePage
 from .views import HomePage, CreateUser
 
 urlpatterns = [
-    path('', homePage, name='home'),
     path('view/', HomePage.as_view(), name='home'),
     path('createuser/', CreateUser.as_view(), name = 'createUser')
 ]
