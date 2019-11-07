f4f960d2b6becd3e3fc310a2d57b101aa75d43f7
Thomas Ruffino <thomasruffino1@gmail.com>
2019-11-07 08:49:24 -0500

    added create user feature to work

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
 
 # Create your views here.
-def homePage(req):
-    return HttpResponse('First App')
 
 class HomePage (TemplateView):
     template_name = 'home.html'
 
-class CreateUser (TemplateView):
+class CreateUser (generic.CreateView):
     template_name = 'createUser.html'
+    success_url = reverse_lazy('login')
+    form_class = UserCreationForm
